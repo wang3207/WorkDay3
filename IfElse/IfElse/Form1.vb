071b1066a0b6410ce39c88957fd618a32b4e494f
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:14:46 -0500

    Solved the problem

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,16 +5,28 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
+ 
+
 
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
+
+        ' checkpoint
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type the numbers")
+            Return
+        End If
+
+        ' clean area 
         num1 = CDbl(st1)
         num2 = CDbl(st2)
-
         Dim result As String
+
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type non-negative numbers!")
+            Return
+        End If
 
         If num1 > num2 Then
             result = "The larger number is " & num1
@@ -25,6 +37,10 @@ Public Class Form1
         End If
 
         txtResult.Text = result
+
+
+
+
     End Sub
 
 End Class
